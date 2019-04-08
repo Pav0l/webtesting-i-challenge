7e0bc14d3ad9b0e66f56eb978a89c366037abcb5
Pav0l <pavol.karas@gmail.com>
2019-04-08 14:27:26 +0200

    Create MVP tests and functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,32 @@ module.exports = {
 };
 
 function succeed(item) {
+  if (item.enhancement < 20) {
+    item.enhancement += 1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  const { enhancement } = item;
+
+  if (enhancement < 15) {
+    item.durability -= 5;
+  }
+
+  if (enhancement >= 15) {
+    item.durability -= 10;
+  }
+
+  if (enhancement > 16) {
+    item.enhancement -= 1;
+  }
+
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,63 @@
 const enhancer = require('./enhancer.js');
 // test away!
+const { succeed, fail, repair, get } = enhancer;
+
+const defaultItem = {
+  name: 'Rusty Sword',
+  durability: 50,
+  enhancement: 5,
+};
+
+const maxEnhancedItem = {
+  name: 'Mega Sword',
+  durability: 50,
+  enhancement: 20,
+};
+
+describe('enhancers', () => {
+  describe('repair item', () => {
+    it('returns new item with 100 durability', () => {
+      expect(repair(defaultItem)).toHaveProperty('durability', 100);
+    });
+  });
+
+  describe('succesfully enhance item', () => {
+    it('increases enhancement level by 1', () => {
+      const increasedEnhancement = defaultItem.enhancement + 1;
+      expect(succeed(defaultItem)).toHaveProperty(
+        'enhancement',
+        increasedEnhancement
+      );
+    });
+
+    it('should not enhance above 20', () => {
+      expect(succeed(maxEnhancedItem)).toHaveProperty('enhancement', 20);
+    });
+  });
+
+  describe('failed item enhancement', () => {
+    it('decrease durability by 5', () => {
+      const decreasedDurability = defaultItem.durability - 5;
+      expect(fail(defaultItem)).toHaveProperty(
+        'durability',
+        decreasedDurability
+      );
+    });
+
+    it('decrease durability by 10', () => {
+      const decreasedDurability = maxEnhancedItem.durability - 10;
+      expect(fail(maxEnhancedItem)).toHaveProperty(
+        'durability',
+        decreasedDurability
+      );
+    });
+
+    it('decrease enhancement by 1', () => {
+      const decreasedEnhancement = maxEnhancedItem.enhancement - 1;
+      expect(fail(maxEnhancedItem)).toHaveProperty(
+        'enhancement',
+        decreasedEnhancement
+      );
+    });
+  });
+});
